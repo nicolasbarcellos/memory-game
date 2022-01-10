0c0b1dc2786e0c77c05ac47b22cfba5ded702813
Nicolas <nicolasbarcellos95@gmail.com>
2022-01-10 13:25:01 -0300

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import "./Card.css";
+
+export default function Card({ card, handleChoice, flipped, disabled }) {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card);
+    }
+  };
+
+  return (
+    <div className="card">
+      <div className={flipped ? "flipped" : ""}>
+        <img src={card.src} alt="card front" className="front" />
+        <img
+          onClick={handleClick}
+          src="/img/cover.png"
+          alt="card back"
+          className="back"
+        />
+      </div>
+    </div>
+  );
+}
